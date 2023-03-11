5fc008fa4116ea3fb8e3103d46b48faf8ef19607
hendisantika <hendisantika@yahoo.co.id>
2023-03-11 14:42:29 +0700

    #20 - Add AbstractPostgresJupiterTest ✏️📗:octocat::two:

---
+++ src/test/groovy/com/hendisantika/AbstractPostgresJupiterTest.java
@@ -0,0 +1,46 @@
+package com.hendisantika;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testcontainer-flyway
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/11/23
+ * Time: 14:42
+ * To change this template use File | Settings | File Templates.
+ */
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+/**
+ * Starts a single PostgreSQL instance per JVM. All tests run on a single JVM reuse this PostgreSQL instance. Reusing
+ * the database instance saves considerable time, especially if your test database starts slower than PostgreSQL.
+ * <p/>
+ * See the <a href="https://www.testcontainers.org/test_framework_integration/manual_lifecycle_control/">Testcontainers
+ * documentation</a> for details about singleton containers.
+ */
+public class AbstractPostgresJupiterTest implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:15.2");
+
+    static {
+        postgres.start();
+    }
+
+    @Override
+    public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
+        TestPropertyValues
+                .of(
+                        "spring.datasource.url=" + postgres.getJdbcUrl(),
+                        "spring.datasource.password=" + postgres.getPassword(),
+                        "spring.datasource.username=" + postgres.getUsername(),
+                        "spring.flyway.clean-disabled=false"
+                )
+                .applyTo(applicationContext);
+    }
+
+}
